c11e9d2f0d6fca9073f82bf529f07a5c612cc6dc
Vishvendra Singh <ddmteetu@gmail.com>
2021-05-11 17:43:42 +0530

    Submitting Events

--- applications/turnout/src/firebaseApp.js
+++ applications/turnout/src/firebaseApp.js
@@ -3,3 +3,4 @@ import firebase from 'firebase'
 const firebaseConfig = {};
 
 export const firebaseApp = firebase.initializeApp(firebaseConfig)
+export const eventsRef = firebaseApp.database().ref().child('events')
